53167d7056a4aa636891d646ff29713d6ca992cf
billdex <pc.luoxin@foxmail.com>
2023-06-07 03:08:13 +0800

    优化通知信息格式, 降低通知频次

--- app/components/market-monitor.tsx
+++ app/components/market-monitor.tsx
@@ -1,26 +1,29 @@
 import {MonitorConfigData} from "@/app/data/monitor-config";
 import useSWR from "swr";
 import {GoodsExchangeItem} from "@/app/data/market";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 interface IMarketMonitor {
     monitorConfig: MonitorConfigData
 }
 
 export default function MarketMonitor({monitorConfig}: IMarketMonitor) {
-    const [updateTime, setUpdateTime] = useState<Date|null>(null)
+    const [updateTime, setUpdateTime] = useState<Date>(new Date())
     const {
         data,
         error,
         isLoading
     } = useSWR<GoodsExchangeItem[]>(`https://www.simcompanies.com/api/v3/market/0/${monitorConfig.id}/`,
-        (url) => fetch(url).then((res) => {
+        (url) => fetch(url, {
+            referrer: "",
+            referrerPolicy: "no-referrer",
+        }).then((res) => {
             setUpdateTime(new Date())
             return res.json()
         }), {
             revalidateOnFocus: false,
             refreshWhenHidden: true,
-            refreshInterval:   10*1000,
+            refreshInterval:   12*1000,
         });
     // 当符合监控策略条件的时候，发送通知
     const notifyTextList = new Array<string>()
@@ -35,15 +38,16 @@ export default function MarketMonitor({monitorConfig}: IMarketMonitor) {
             }
         }
         if (goodsCount > 0 && goodsCount >= strategy.count) {
-            notifyTextList.push(`Q${data?.[0].quality} @${data?.[0].price} ${goodsCount}u`)
+            notifyTextList.push(`Q${strategy.quality}+  |  @${data?.[0].price}/${strategy.price}  |  ${goodsCount}u`)
         }
     }))
-    if (notifyTextList.length > 0) {
-        const notifyText = notifyTextList.join("\n")
+    if (notifyTextList.length > 0 && (new Date().getTime() - updateTime?.getTime()) > 25) {
+        const notifyTitle = `re-${monitorConfig.id} ${monitorConfig.name} ${monitorConfig.name_en}`
+        const notifyBody = notifyTextList.join("\n")
         if (Notification.permission === "granted") {
-            new Notification(monitorConfig.name, {body: notifyText})
+            new Notification(notifyTitle, {body: notifyBody})
         } else {
-            Notification.requestPermission().then(() => new Notification(monitorConfig.name, {body: notifyText}))
+            Notification.requestPermission().then(() => new Notification(notifyTitle, {body: notifyBody}))
         }
     }
     return (
